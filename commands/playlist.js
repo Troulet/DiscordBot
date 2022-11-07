443943f900d3f8f05eade94bf18fe214921929e5
Tristan ROULET <tristan.roulet@viacesi.fr>
2022-11-07 13:30:56 +0100

    Link Playlist Fall 2022

--- commands/playlist.js
+++ commands/playlist.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('playlist')
 		.setDescription('Envoie la playlist de la saison'),
 	async execute(interaction) {
-		await interaction.reply({ content: 'Voici le lien vers la playlist de la saison : https://kodcast.com/w/3RKeEUXuAgxDuXcBJ5aghu', ephemeral: false });
+		await interaction.reply({ content: 'Voici le lien vers la playlist de la saison : https://kodcast.com/w/9EF6ugw3dS9sDkvtrsVXSS', ephemeral: false });
 	},
 };
