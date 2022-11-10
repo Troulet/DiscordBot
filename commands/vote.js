07640784435d7633ed6825a92978bc9832c286a3
Tristan ROULET <tristan.roulet@viacesi.fr>
2022-11-10 18:20:41 +0100

    RegExp change

--- commands/vote.js
+++ commands/vote.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require("discord.js");
 const buenoId = '229241724130557952'
 const trouletId = '321215339079925761'
-const re = new RegExp('\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+')
+const re = /\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+\D*\d+/gm
 let voteList = []
 
 module.exports = {
