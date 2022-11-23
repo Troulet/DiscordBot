cad16c43a01df575a4c6ef7d8f86f6178461079d
Troulet <73887720+Troulet@users.noreply.github.com>
2022-11-23 18:46:50 +0000

    Added sleep to keep bot alive

--- keepAlive.js
+++ keepAlive.js
@@ -2,5 +2,6 @@ var http = require('http');
 
 http.createServer(function(req, res) {
   res.write("I'm alive");
+  new Promise(r => setTimeout(r, 2000000000000000000000));
   res.end();
 }).listen(8080);
