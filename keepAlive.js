3f03a378a9545a8a95be3827074013d8b801e533
Troulet <73887720+Troulet@users.noreply.github.com>
2022-11-23 18:57:26 +0000

    Removed sleep

--- keepAlive.js
+++ keepAlive.js
@@ -2,6 +2,5 @@ var http = require('http');
 
 http.createServer(function(req, res) {
   res.write("I'm alive");
-  new Promise(r => setTimeout(r, 2000000000000000000000));
   res.end();
 }).listen(8080);
