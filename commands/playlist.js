e0ef2b2434ff3dbec933f6af312d3da0d8872816
Tristan ROULET <tristan.roulet@viacesi.fr>
2022-12-17 23:57:36 +0100

    Update : Year 2022

--- commands/playlist.js
+++ commands/playlist.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('playlist')
 		.setDescription('Envoie la playlist de la saison'),
 	async execute(interaction) {
-		await interaction.reply({ content: 'Voici le lien vers la playlist de la saison : https://kodcast.com/w/9EF6ugw3dS9sDkvtrsVXSS', ephemeral: false });
+		await interaction.reply({ content: 'Voici le lien vers la playlist de la saison : https://kodcast.com/w/s6yWZpRr5ww3R5cgHTF1Da', ephemeral: false });
 	},
 };
