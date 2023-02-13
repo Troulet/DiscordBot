c0694663cca333cea2d71e5adcceb2f08590fce1
Tristan ROULET <tristan.roulet@viacesi.fr>
2023-02-13 23:23:48 +0100

    Update playlist link

--- commands/playlist.js
+++ commands/playlist.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('playlist')
 		.setDescription('Envoie la playlist de la saison'),
 	async execute(interaction) {
-		await interaction.reply({ content: 'Voici le lien vers la playlist de la saison : https://kodcast.com/w/aPo1XhJrD5nyfW7p5ihA12', ephemeral: false });
+		await interaction.reply({ content: 'Voici le lien vers la playlist de la saison : https://kodcast.com/w/qethJR8weJdGQT5j4Whfvr', ephemeral: false });
 	},
 };
